7590768feaf08c4d88f82b49b8ef8d79236f2016
Am3Chan <118872433+Am3Chan@users.noreply.github.com>
2023-05-14 09:27:46 +0900

    Sig Update

--- r5script/dllmain.cpp
+++ r5script/dllmain.cpp
@@ -121,7 +121,7 @@ bool __stdcall DllMain(const HMODULE module, const DWORD reason, LPVOID)
 	DetourTransactionBegin();
 
 	original_load_rson = reinterpret_cast<SQInteger(*)(const SQChar*)>(find_pattern(skCrypt("4C 8B DC 49 89 5B 08 57 48 81 EC A0 00 00 00 33"), nullptr));
-	original_load_script = reinterpret_cast<SQBool(*)(void*, const SQChar*, const SQChar*, SQInteger)>(find_pattern(skCrypt("48 8B C4 4C 89 40 18 55 41"), nullptr));
+	original_load_script = reinterpret_cast<SQBool(*)(void*, const SQChar*, const SQChar*, SQInteger)>(find_pattern(skCrypt("48 8B C4 4C 89 40 18 48 89 48 08 55 41"), nullptr));
 
 	DetourAttach((LPVOID*)&original_load_rson, &load_rson);
 	DetourAttach((LPVOID*)&original_load_script, &load_script);
